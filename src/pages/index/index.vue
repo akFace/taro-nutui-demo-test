dda881231cb9b4cb2fe8cf6702a16627a437ca56
墨迹 <kang558@foxmail.com>
2022-12-23 10:31:20 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+    <text class="iconfont icon-search"></text>
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './index.scss'
+
+export default {
+  setup () {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+}
+</script>
